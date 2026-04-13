5d96eafff427017ef30195ac75ad5d8680e60e05
wtaype <wilder.taype16@gmail.com>
2026-04-13 16:04:43 -0500

    Actualizacion Principal v9.10.10

--- lib/smiles/wii.dart
+++ lib/smiles/wii.dart
@@ -8,17 +8,17 @@ class wii {
   static const String autor = '@wilder.taype';
   static const String by = '@wilder.taype';
   static const String link = 'https://wtaype.github.io/';
-  static const String version = 'v11.10';
+  static const String version = 'v9.10';
 }
 
 /** ACTUALIZACIÓN PRINCIPAL ONE DEV [MAIN] (1)
-git add . ; git commit -m "Actualizacion Principal v11.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v9.10.10" ; git push origin main
 
 //  Actualizar versiones de seguridad [TAG NUEVO] (2)
-git tag v11 -m "Version v11" ; git push origin v11
+git tag v9 -m "Version v9" ; git push origin v9
 
 // Actualizar versiones de seguridad [TAG REMPLAZO] (3)
-git tag -d v11 ; git tag v11 -m "Version v11 actualizada" ; git push origin v11 --force
+git tag -d v9 ; git tag v9 -m "Version v9 actualizada" ; git push origin v9 --force
 
 // Compiplacion + exe windows
 flutter build windows --release
